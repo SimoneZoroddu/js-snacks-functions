e8d817d382db31bfa3e3c5eec0f5bf960fdcd8dc
SimoneZoroddu <lenviur@gmail.com>
2026-02-10 14:34:11 +0100

    half of the third exercise

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,6 +4,45 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/*
+function name_char(array) {
+    let single_name = []
+    for (let i = 0; i < array.length; i++) {
+        const single_array = array[i];
+        if (single_array.indexOf('A') + 1) { //// +1 perche indexof partendo da 0 da falso
+            single_name.push(single_array)
+        }
+    }
+    return single_name
+}
+*/
+
+// console.log(name_char(names));
+
+/// penso si puo fare anche con 2 funzioni una per l array e una per il carattere
+
+function name_array(array) {
+    let single_name = []
+    for (let i = 0; i < array.length; i++) {
+        const single_array = array[i];
+        single_name.push(single_array)
+    }
+    return single_name
+}
+
+
+function char_in_names(char) {
+    let single_char = []
+    for (let i = 0; i < char.length; i++) {
+        const element = char[i];
+        
+    }
+}
+
+
+
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
