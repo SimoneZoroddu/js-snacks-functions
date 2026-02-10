abbaccb023a69185b0cb62dd70a76652ebc75afc
SimoneZoroddu <lenviur@gmail.com>
2026-02-10 14:55:12 +0100

    exercise 3 complete

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -21,6 +21,8 @@ function name_char(array) {
 
 /// penso si puo fare anche con 2 funzioni una per l array e una per il carattere
 
+
+/// divido i vari elementi dentro larray
 function name_array(array) {
     let single_name = []
     for (let i = 0; i < array.length; i++) {
@@ -30,23 +32,27 @@ function name_array(array) {
     return single_name
 }
 
+//console.log(name_array(names));
+//creo una variabile con dentro la function e l array specificato
+let prova = name_array(names)
 
+//creo una funzione che cerca solo la lettera che voglio
 function char_in_names(char) {
     let single_char = []
-    for (let i = 0; i < char.length; i++) {
-        const element = char[i];
-        
+    for (let i = 0; i < prova.length; i++) {
+        const element = prova[i];  
+        if (element.indexOf(char) + 1) { //// +1 perche indexof partendo da 0 da falso
+            single_char.push(element)
+        }
     }
+
+    return single_char
 }
-
-
-
-
-
-
 
 // Invoca la funzione qui e stampa il risultato in console
 
+// chiamo la funzione con la lettera che voglio
+console.log(char_in_names('A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
