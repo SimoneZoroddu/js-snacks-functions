1f748f67a8d977dae877101b63cc0c44a42dfcdd
SimoneZoroddu <lenviur@gmail.com>
2026-02-10 14:06:35 +0100

    second exercise

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,29 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+console.log(names);
 
 // Dichiara la funzione qui.
-
+function start_array(array) {
+    let  single_name = []
+    for (let i = 0; i < array.length; i++) {
+        const single_array = array[i];
+        single_name.push(single_array)
+    }
+    let start_char = []
+    for (let i = 0; i < single_name.length; i++) {
+        const single_char = single_name[i];
+        start_char.push(single_char[0])
+    }
+    return start_char
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(start_array(names));
 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+//info
