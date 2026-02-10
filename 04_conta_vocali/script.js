01fd14835971da249d568d3644689bcf7d202200
SimoneZoroddu <lenviur@gmail.com>
2026-02-10 16:04:52 +0100

    exercise 4 and bonus

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,30 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+let vocals = ['a', 'e', 'i', 'o', 'u']
 
-// Invoca la funzione qui e stampa il risultato in console
 
+function vocal(words) {
+    for (let i = 0; i < words.length; i++) {
+        const single_char = words[i];
+        for (let i = 0; i < vocals.length; i++) {
+            const element = vocals[i];
+            let vocals_false = false
+            if (single_char == element) {
+                vocals_false += true
+            }
+            if (vocals_false) {
+                return single_char
+            }   
+        }
+    }
+}
+
+// // Invoca la funzione qui e stampa il risultato in console
+
+console.log(vocal(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,12 +7,32 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
+const d = new Date();
+let hour = d.getHours();
+
+console.log(hour);
+
 
 // Dichiara la funzione qui.
+function name_string(names) {
+    let namestring = ''
+    for (let i = 0; i < names.length; i++) {
+        const char = names[i]
+        namestring += char
+        if (hour < 13) {
+            return `Buongiorno ${names}`
+        } else if (hour > 17) {
+            return `Buonasera ${names}`
+        } else {
+            return `Buon Pomeriggio ${names}`
+        }
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(name_string(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
